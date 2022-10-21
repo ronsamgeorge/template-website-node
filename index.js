c01561b225a2b4dc5e66e6c0ef483f7874e90640
Ronald George <ronaldsamgeorge@gmail.com>
2022-10-21 23:34:26 +0530

    Refactor header code and implement redirect routing

--- index.js
+++ index.js
@@ -6,32 +6,38 @@ const server = http.createServer(function(req, res){
     console.log(req.url);
 
     let path = "./views/"
-    let resCode = 200;
+    res.setHeader('Content-Type', 'text/html');
 
     switch(req.url){
         case '/':
             path +='index.html';
+            res.statusCode = 200;
             break;
         case '/about.html':
-            path += 'about.html'
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+
+        case '/about-me.html':
+            res.statusCode = 301;
+            res.setHeader('location', '/about.html');
             break;
 
         case '/contact.html':
             path += 'contact.html'; 
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
-            resCode = 404;
+            res.statusCode = 404;
             break;
     }
 
     fs.readFile(path, 'utf-8', (err,data) => {
-
     if(err){
         console.log(err);
         res.end();
     }
-    res.writeHead(resCode,{"Content-Type" : "text/html"});
     res.end(data);
     });
     
